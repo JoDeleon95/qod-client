8ae5a3d9fca35da16e37504e2e5e84c5ab153ec9
“JoDeLeon95” <JoDeLeon95@gmail.com>
2019-04-03 07:56:25 -0600

    Added google sign-out

--- app/src/main/java/edu/cnm/deepdive/qod/controller/LoginActivity.java
+++ app/src/main/java/edu/cnm/deepdive/qod/controller/LoginActivity.java
@@ -1,9 +1,9 @@
 package edu.cnm.deepdive.qod.controller;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.Toast;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -45,7 +45,6 @@ public class LoginActivity extends AppCompatActivity {
         switchToMain();
       } catch (ApiException e) {
         Toast.makeText(this, R.string.sign_in_error, Toast.LENGTH_LONG).show();
-
       }
     }
   }
@@ -58,7 +57,13 @@ public class LoginActivity extends AppCompatActivity {
   private void switchToMain() {
     Intent intent = new Intent(this, MainActivity.class);
     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-
+    startActivity(intent);
   }
 
 }
+
+
+
+
+
+
